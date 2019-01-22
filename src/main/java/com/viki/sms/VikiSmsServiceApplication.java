e76dee92e6170a52be60acefabfc468c246381a5
Viki Chen <viki@chinadep.com>
2019-01-22 13:19:29 +0800

    1.短信微服务

---
+++ src/main/java/com/viki/sms/VikiSmsServiceApplication.java
@@ -0,0 +1,14 @@
+package com.viki.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikiSmsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VikiSmsServiceApplication.class, args);
+    }
+
+}
+
